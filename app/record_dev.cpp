fc1fc95367538ebfbb083195d2dcdf82284eacb8
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-12-21 01:49:32 +0900

    chore: Add periodic while

---
+++ app/record_dev.cpp
@@ -0,0 +1,77 @@
+#include <signal.h>
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+#include "efp.hpp"
+#include "logger.hpp"
+#include "pigpio.h"
+
+#include "hal.hpp"
+
+using namespace efp;
+using namespace std::chrono;
+
+// Step of voltage should be around 0.1 %
+constexpr double max_voltage_v = 3.3;
+
+// HAL input
+
+void signal_handler(int signum)
+{
+    info("Signal {} received in main thread", signum);
+
+    debug("Terminatig PIGPIO.");
+    gpioTerminate();
+
+    info("Terminating program.");
+    exit(signum);
+}
+
+int main()
+{
+    // Block all signals for this thread (main thread)
+    sigset_t set;
+    sigfillset(&set);
+    pthread_sigmask(SIG_BLOCK, &set, NULL);
+
+    // Spawn threads
+    Logger::set_log_level(LogLevel::Trace);
+
+    info("Starting MEU3014 2nd project.");
+
+    debug("Initializing PIGPIO.");
+    if (gpioInitialise() < 0)
+    {
+        fatal("Failed to initialize PIGPIO.");
+        return -1;
+    }
+
+    debug("Setting HALs.");
+    auto &encoder = Encoder::instance();
+    auto &motor = DcMotor::instance();
+
+    // Unblocking signals in the main thread
+    sigemptyset(&set);
+    pthread_sigmask(SIG_SETMASK, &set, NULL);
+
+    // Register signal handler for main thread
+    signal(SIGINT, signal_handler);
+
+    periodic_while(
+        seconds(1),
+        [](const auto time)
+        { return time < seconds(10); },
+        [](const auto time)
+        { info("Periodic time check: {}", time); });
+
+    // Clean up
+    debug("Terminatig PIGPIO.");
+    gpioTerminate();
+
+    info("Good bye");
+    std::this_thread::sleep_for(milliseconds(1000));
+    return 0;
+}
--- include/hal.hpp
+++ include/hal.hpp
@@ -22,13 +22,42 @@
 
 int now_us();
 
+// periodic_while
+template <typename Duration, typename F, typename G>
+void periodic_while(Duration period, const F &keep, const G &task)
+{
+    using Clock = std::chrono::steady_clock;
+
+    const auto start_timepoint = Clock::now();
+    auto next_timepoint = start_timepoint;
+
+    while (true)
+    {
+        auto now = Clock::now();
+        const auto elapsed_time = now - start_timepoint;
+
+        if (!keep(elapsed_time))
+            break;
+
+        task(elapsed_time);
+
+        next_timepoint += period;
+
+        std::this_thread::sleep_until(next_timepoint);
+        while (now < next_timepoint)
+        {
+        }
+    }
+}
+
 // Encoder
 
 void isr_encoder_a(int gpio, int level, uint32_t tick);
 void isr_encoder_b(int gpio, int level, uint32_t tick);
 
-class Encoder{
-public: 
+class Encoder
+{
+public:
     static Encoder &instance()
     {
         static Encoder inst;
@@ -39,7 +68,7 @@ public:
     void isr_b();
     int operator()();
 
- private:
+private:
     Encoder();
 
     std::mutex mtx_;
@@ -66,19 +95,25 @@ private:
 };
 
 template <typename Sequence>
-void print_csv(const std::string& filename, const Sequence& seq) {
-    std::FILE* file = std::fopen(filename.c_str(), "w");
+void print_csv(const std::string &filename, const Sequence &seq)
+{
+    std::FILE *file = std::fopen(filename.c_str(), "w");
 
-    if (file) {
-        for (size_t i = 0; i < seq.size(); ++i) {
-            if (i > 0) {
+    if (file)
+    {
+        for (size_t i = 0; i < seq.size(); ++i)
+        {
+            if (i > 0)
+            {
                 fmt::print(file, ",");
             }
             fmt::print(file, "{}", seq[i]);
         }
         fmt::print(file, "\n");
         std::fclose(file);
-    } else {
+    }
+    else
+    {
         fmt::print(stderr, "Failed to open the file: {}\n", filename);
     }
 }
